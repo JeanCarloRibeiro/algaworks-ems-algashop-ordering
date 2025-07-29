f9b0e8fcca75fb4e2b332082c579c9c4aa579409
Jean Carlo Ribeiro <jean.ribeiro@valecard.com.br>
2025-07-28 21:23:17 -0300

    Criando projeto do microsserviço de Ordering

---
+++ src/test/java/com/algashop/ordering/AlgashopOrderingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algashop.ordering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgashopOrderingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
