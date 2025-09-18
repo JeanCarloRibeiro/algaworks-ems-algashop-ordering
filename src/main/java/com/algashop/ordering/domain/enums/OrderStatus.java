655793c967a0fac5112c4a969565685cf7973953
Jean Carlo Ribeiro <jean.ribeiro@valecard.com.br>
2025-09-18 12:10:24 -0300

    Implementando Aggregate de Order

---
+++ src/main/java/com/algashop/ordering/domain/enums/OrderStatus.java
@@ -0,0 +1,9 @@
+package com.algashop.ordering.domain.enums;
+
+public enum OrderStatus {
+  DRAFT,
+  PLACED,
+  PAID,
+  READY,
+  CANCELED
+}
---
+++ src/main/java/com/algashop/ordering/domain/enums/PaymentMethod.java
@@ -0,0 +1,6 @@
+package com.algashop.ordering.domain.enums;
+
+public enum PaymentMethod {
+  CREDIT_CARD,
+  GATEWAY_BALANCE
+}
